365d703284b572991dbe19dba84a623b521c951e
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2021-11-08 13:46:23 +0100

    codice esempio

---
+++ ECommerce/ECommerce/Shared/IGestioneCarrello.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared
+{
+    public interface IGestioneCatalogo
+    {
+        Catalogo GetCatalogoCorrente();
+      //  void CreaCatalogo(Catalogo nuovoCatalogo);
+
+    }
+
+    public class GestioneCatalogo : IGestioneCatalogo
+    {
+        public Catalogo GetCatalogoCorrente()
+        {
+            return new Catalogo
+            {
+                Prodotti = new List<Prodotto>
+            {
+                new Prodotto { Id = 1, Name ="Prod1"},
+                new Prodotto { Id = 2, Name ="Prod2"}
+            }
+            };
+        }
+    }
+}
---
+++ ECommerce/ECommerce/Shared/Ordine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared
+{
+    public class Ordine
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        List<RigaOrdine> RigheOrdine { get; set; } = new List<RigaOrdine>();
+    }
+
+    public  class RigaOrdine
+    {
+        public int Id { get; set; }
+        public int Quantità { get; set; }
+        public int IdProdotto { get; set; }
+    }
+
+    public class Prodotto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Catalogo
+    {
+        public List<Prodotto> Prodotti { get; set; }
+    }
+
+  
+
+}
